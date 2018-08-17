e7e393eda1806fabd270e1aa01e8b5958ba77060
heipaklam <clam@paciolan.com>
2018-08-17 11:31:00 -0700

    add modules for build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './components/App';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-ReactDom.render(
-<Provider>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-</Provider>
-, 
-document.getElementById('app'));
+  </Provider>,
+  document.getElementById("app")
+);
